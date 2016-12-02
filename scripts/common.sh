e64676c1af812d46bf138554cbe1e03228bb8955
Declan Shanaghy <dshanaghy@splunk.com>
2016-12-02 08:54:42 -0800

    Transitioned to polarishq/skel-builder base image

--- scripts/common.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pkg_root=$(cat ./vendor/vendor.json | grep rootPath | awk '{split($0,array,": ")} END{print array[2]}' | awk '{split($0,array,"\"")} END{print array[2]}')
-
-export OUR_PKGS=$(govendor list -no-status +local,^prog | grep -v client | grep -v restapi | grep -v models | grep -v mock | grep -v test)
-export OUR_DIRS="${OUR_PKGS//$pkg_root\/}"
-
