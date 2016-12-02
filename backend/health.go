c7860231a72fa3003d0181401004252f1e493199
Declan Shanaghy <dshanaghy@splunk.com>
2016-12-02 09:44:52 -0800

    Remove explicit port mapping

--- backend/health.go
+++ backend/health.go
@@ -4,6 +4,10 @@ import (
 	"github.com/declanshanaghy/bbqberry/framework"
 	"github.com/declanshanaghy/bbqberry/framework/log"
 	"github.com/declanshanaghy/bbqberry/models"
+	"github.com/declanshanaghy/bbqberry/influx"
+	"github.com/declanshanaghy/bbqberry/framework/error"
+	"fmt"
+	"github.com/declanshanaghy/bbqberry/influx/example"
 )
 
 // Health performs all internal health checks to ensure all systems are functioning
@@ -20,30 +24,30 @@ func Health() (m models.Health, err error) {
 	si.Version = &framework.Constants.Version
 	m.ServiceInfo = si
 
-	//client, err := influx.GetDefaultClient()
-	//if err != nil {
-	//	e := new(models.Error)
-	//	code := errorcodes.ErrInfluxUnavailable
-	//	e.Code = &code
-	//	e.Message = fmt.Sprintf("%s %s", errorcodes.GetText(*e.Code), err)
-	//	m.Error = e
-	//	return m, nil
-	//}
-	//
-	//tags := map[string]string{"service": *si.Name}
-	//fields := map[string]interface{}{
-	//	"version": si.Version,
-	//}
-	//
-	//_, err = influxexample.WriteExamplePoint(client, "health", tags, fields)
-	//if err != nil {
-	//	e := new(models.Error)
-	//	code := errorcodes.ErrInfluxWrite
-	//	e.Code = &code
-	//	e.Message = fmt.Sprintf("%s %s", errorcodes.GetText(*e.Code), err)
-	//	m.Error = e
-	//	return m, nil
-	//}
+	client, err := influx.GetDefaultClient()
+	if err != nil {
+		e := new(models.Error)
+		code := errorcodes.ErrInfluxUnavailable
+		e.Code = &code
+		e.Message = fmt.Sprintf("%s %s", errorcodes.GetText(*e.Code), err)
+		m.Error = e
+		return m, nil
+	}
+
+	tags := map[string]string{"service": *si.Name}
+	fields := map[string]interface{}{
+		"version": si.Version,
+	}
+
+	_, err = influxexample.WriteExamplePoint(client, "health", tags, fields)
+	if err != nil {
+		e := new(models.Error)
+		code := errorcodes.ErrInfluxWrite
+		e.Code = &code
+		e.Message = fmt.Sprintf("%s %s", errorcodes.GetText(*e.Code), err)
+		m.Error = e
+		return m, nil
+	}
 
 	healthy = true
 	return m, nil
