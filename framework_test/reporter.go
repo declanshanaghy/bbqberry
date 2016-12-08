f19bfab000a830f6bb1e20e1886bc7fa5d8349d2
Declan Shanaghy <dshanaghy@splunk.com>
2016-12-07 22:33:19 -0800

    Fix code style errors

--- framework_test/reporter.go
+++ framework_test/reporter.go
@@ -6,12 +6,15 @@ import (
 	. "github.com/onsi/ginkgo"
 )
 
-type GinkgoTestReporter struct{}
+type ginkgoTestReporter struct{}
 
-func (g GinkgoTestReporter) Errorf(format string, args ...interface{}) {
+
+// Fail causes the test to fail with the given formatted string reported as the failure reason
+func (g ginkgoTestReporter) Errorf(format string, args ...interface{}) {
 	Fail(fmt.Sprintf(format, args))
 }
 
-func (g GinkgoTestReporter) Fatalf(format string, args ...interface{}) {
+// Fatalf causes the test to fail with the given formatted string reported as the failure reason
+func (g ginkgoTestReporter) Fatalf(format string, args ...interface{}) {
 	Fail(fmt.Sprintf(format, args))
 }
