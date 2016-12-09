aa7107b1624df3e8fec48541a827927aac71dacd
Declan Shanaghy <dshanaghy@splunk.com>
2016-12-08 22:27:34 -0800

    Disable real temp readings

--- hardware/temperature.go
+++ hardware/temperature.go
@@ -58,10 +58,11 @@ func (s *temperatureArray) GetTemperatureReading(probe int32) (*TemperatureReadi
 	}
 
 	log.Debugf("action=GetTemp probe=%d", probe)
-	v, err := s.adc.AnalogValueAt(int(probe))
-	if err != nil {
-		panic(err)
-	}
+	// v, err := s.adc.AnalogValueAt(int(probe))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	v := 2
 	k, c, f := convertVoltToTemp(v)
 	return &TemperatureReading{
 		Probe:      probe,
