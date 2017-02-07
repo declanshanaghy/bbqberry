9b0fbce3e8ee26abd6e9c5359887829e199e9408
Declan Shanaghy <dshanaghy@splunk.com>
2017-02-06 20:30:07 -0800

    D3 service working

--- static/bbqberry/app/views/glance/glance.js
+++ static/bbqberry/app/views/glance/glance.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('bbqberry.glance', ['ngRadialGauge', 'ngRoute', 'emguo.poller'])
+angular.module('bbqberry.glance', ['d3', 'ngRadialGauge', 'ngRoute', 'emguo.poller'])
 
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/glance', {
@@ -9,59 +9,47 @@ angular.module('bbqberry.glance', ['ngRadialGauge', 'ngRoute', 'emguo.poller'])
         })
     }])
 
-    .controller('GlanceController', ['$scope', '$http', 'poller', function ($scope, $http, poller) {
-        $scope.ranges = [
-            {
-                min: 0,
-                max: 100,
-                color: '#22FF44'
-            },
-            {
-                min: 100,
-                max: 200,
-                color: '#41DF3F'
-            },
-            {
-                min: 200,
-                max: 300,
-                color: '#61BF3A'
-            },
-            {
-                min: 300,
-                max: 400,
-                color: '#80A035'
-            },
-            {
-                min: 400,
-                max: 500,
-                color: '#A08030'
-            },
-            {
-                min: 500,
-                max: 600,
-                color: '#BF612B'
-            },
-            {
-                min: 600,
-                max: 700,
-                color: '#DF4126'
-            },
-            {
-                min: 700,
-                max: 800,
-                color: '#FF2222'
-            }
-        ];
-        $scope.upperLimit = $scope.ranges[$scope.ranges.length - 1].max;
-        $scope.majorGraduations = $scope.ranges.length;
+    .controller('GlanceController', ['$scope', '$http', 'poller', 'd3Service',
+        function ($scope, $http, poller, d3Service) {
+            d3Service.d3().then(function (d3) {
 
-        var myPoller = poller.get('/api/v1/temperatures/probes', {
-            action: 'get',
-            delay: 5000
-        });
+                var grads = d3.scale.linear()
+                    .range([0, 800])
+                    .interpolate(d3.interpolateNumber);
 
-        myPoller.promise.then(null, null, function(response) {
-            $scope.probes = response['data'];
-        });
+                var min = 0;
+                var max = 800;
+                var step = 100;
+                var steps = (max - min) / step;
 
-    }]);
+                var color = d3.scale.linear()
+                    .range(["darkblue", "red"])
+                    .interpolate(d3.interpolateHcl);
+
+                $scope.ranges = [];
+                var pos = 0;
+                for (var i = min; i < max; i += step) {
+                    pos = i / (max - min);
+                    var mn = grads(pos);
+                    var mx = mn + step;
+                    var c = color(pos);
+                    console.log("mn=" + mn + ", mx=" + mx + ", c=" + c);
+                    $scope.ranges[$scope.ranges.length] = {
+                        min: mn,
+                        max: mx,
+                        color: c
+                    };
+                }
+                $scope.upperLimit = $scope.ranges[$scope.ranges.length - 1].max;
+                $scope.majorGraduations = $scope.ranges.length + 1;
+
+                var myPoller = poller.get('/api/v1/temperatures/probes', {
+                    action: 'get',
+                    delay: 5000
+                });
+
+                myPoller.promise.then(null, null, function (response) {
+                    $scope.probes = response['data'];
+                });
+            });
+        }]);
